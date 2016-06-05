c64c734e766e4ca84aa40c6e81f6968ea1232637
Suhil Yaftaly <suhail278@gmail.com>
2016-06-05 13:47:55 -0400

    Check for AKP

--- app/src/main/java/com/example/suhail/onmyway/MainActivity.java
+++ app/src/main/java/com/example/suhail/onmyway/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.suhail.onmyway;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.GoogleMap;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +20,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (servicesOK()) {
+            Toast.makeText(this, "Ready to map!", Toast.LENGTH_SHORT).show();
+        } else {
+
+        }
+    }
+
+    //check to see if the google services are ok
+    public boolean servicesOK() {
+
+        int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        // handle 3 different possibility
+        if (isAvailable == ConnectionResult.SUCCESS) { //everything ok, user can make mapping request
+            return true;
+        } else if (GooglePlayServicesUtil.isUserRecoverableError(isAvailable)) { //error user can do something about
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(isAvailable, this, ERROR_DIALOG_REQUEST);
+            dialog.show(); //google play services lib delivers correct dialog box telling user what to do
+        } else { //something wrong, user cant do anything about
+            Toast.makeText(this, "Can't connect to mapping service", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
