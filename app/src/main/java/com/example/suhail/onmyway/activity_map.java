1acf380f8cd7e3c1f5d52ad64d9999b3c97caab1
Suhil Yaftaly <suhail278@gmail.com>
2016-07-13 20:35:54 -0400

    Added Search icon button to the keyboard

--- app/src/main/java/com/example/suhail/onmyway/activity_map.java
+++ app/src/main/java/com/example/suhail/onmyway/activity_map.java
@@ -8,8 +8,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -34,6 +37,7 @@ public class activity_map extends AppCompatActivity implements GoogleApiClient.C
     private GoogleApiClient mLocationClient;
     private Marker addressMarker, myLocationMarker;
 //    private LocationListener mListener; //will be used for later
+    private EditText searchEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +65,25 @@ public class activity_map extends AppCompatActivity implements GoogleApiClient.C
         } else {
             Toast.makeText(this, "Map not connected!", Toast.LENGTH_SHORT).show();
         }
+
+        //view and handle search icon on keyboard
+        searchEditText = (EditText)findViewById(R.id.editTextSearch);
+        searchEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    String searchString = searchEditText.getText().toString();
+                    try {
+                        hideSoftKeyboard(v);
+                        geoLocate(searchString);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
@@ -123,6 +146,7 @@ public class activity_map extends AppCompatActivity implements GoogleApiClient.C
 
         geoLocate(searchString);
     }
+
 
     //set initial state to current location
     public void setCurrentLocation(){
